2788d4aade12ea23cba9abba3345b1c54936af32
steinsphang <824780925@qq.com>
2016-10-19 00:24:59 +0800

    add datepicker for page Before

--- src/components/Before.js
+++ src/components/Before.js
@@ -1,6 +1,44 @@
+import moment from 'moment'
+
 export default {
   name: 'Before',
+  data () {
+    return {
+      pickerValue: new Date(),
+      tempPickerValue: null
+    }
+  },
+  computed: {
+    pickerString () {
+      return moment(this.pickerValue).format('YYYYMMDD')
+    }
+  },
+  methods: {
+    onInput (value) {
+      this.tempPickerValue = value
+    },
+    openPicker () {
+      this.$refs.picker.open()
+    },
+    confirm () {
+      this.pickerValue = this.tempPickerValue
+    }
+  },
   render (h) {
-    return <mt-button>Before</mt-button>
+    return (
+      <div>
+        <mt-cell title={this.pickerString}>
+          <mt-button size="small" type="primary" nativeOn-click={this.openPicker}>选择日期</mt-button>
+        </mt-cell>
+        <mt-datetime-picker
+          ref="picker"
+          type="date"
+          value={this.pickerValue}
+          on-input={this.onInput}
+          endDate={new Date()}
+          on-confirm={this.confirm}>
+        </mt-datetime-picker>
+      </div>
+    )
   }
 }
