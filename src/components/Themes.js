83dde6633f20eae73e7b7ea4ac2708ebd602a996
steinsphang <824780925@qq.com>
2016-10-22 00:44:58 +0800

    get theme content

--- src/components/Themes.js
+++ src/components/Themes.js
@@ -1,21 +1,62 @@
-import { themesGetList } from '../vuex/types'
+import { themesGetList, themesGetContent } from '../vuex/types'
+import Loading from './Loading'
 
 export default {
   name: 'Themes',
+  components: {
+    Loading
+  },
+  data () {
+    return {
+      selectedIndex: -1
+    }
+  },
   computed: {
     themesList () {
       return this.$store.state.Themes.themesList
+    },
+    themesContentList () {
+      return this.$store.state.Themes.themesContentList
+    }
+  },
+  methods: {
+    select (index) {
+      this.$store.dispatch(themesGetContent, index)
+      if (this.selectedIndex === index) {
+        this.selectedIndex = -1
+      } else {
+        this.selectedIndex = index
+      }
+    },
+    goToDetail (id) {
+      this.$router.push(`/detail?id=${id}`)
     }
   },
   render (h) {
     let list = []
     for (let i = 0; i < this.themesList.length; i++) {
       list.push(
-        <mt-cell>
+        <mt-cell nativeOn-click={this.select.bind(null, i)}>
           <img slot="icon" src={'/proxy?url=' + this.themesList[i].thumbnail} width="75" height="75"/>
           <span>{this.themesList[i].name}</span>
         </mt-cell>
       )
+      if (this.selectedIndex === i) {
+        if (this.themesContentList[i]) {
+          for (let j = 0; j < this.themesContentList[this.selectedIndex].length; j++) {
+            list.push(
+              <div>
+                <mt-cell title={this.themesContentList[this.selectedIndex][j].title} nativeOn-click={this.goToDetail.bind(null, this.themesContentList[this.selectedIndex][j].id)}>
+                </mt-cell>
+              </div>
+            )
+          }
+        } else {
+          list.push(
+            <loading></loading>
+          )
+        }
+      }
     }
     return (
       <div>
--- src/vuex/modules/Themes.js
+++ src/vuex/modules/Themes.js
@@ -3,11 +3,15 @@ import * as types from '../types'
 
 export default {
   state: {
-    themesList: []
+    themesList: [],
+    themesContentList: {}
   },
   mutations: {
     [types.themesGetList] (state, payload) {
       state.themesList = payload.themesList
+    },
+    [types.themesGetContent] (state, payload) {
+      Vue.set(state.themesContentList, payload.index, payload.content)
     }
   },
   actions: {
@@ -20,6 +24,19 @@ export default {
       .then((res) => {
         commit(types.themesGetList, {
           themesList: res.others
+        })
+      })
+    },
+    [types.themesGetContent] ({state, commit}, index) {
+      if (state.themesContentList[index]) {
+        return
+      }
+      Vue.http.get(`/proxy?url=http://news-at.zhihu.com/api/4/theme/${state.themesList[index].id}`)
+      .then(res => res.json())
+      .then((res) => {
+        commit(types.themesGetContent, {
+          index,
+          content: res.stories
         })
       })
     }
